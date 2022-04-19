f3a9360d2903ef67bfd4598b2f027b60065a2d64
Ha Thu <tranhathu6989@gmail.com>
2022-04-19 17:12:15 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + '/views/about.html');
+})
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + '/views/gallety.html');
+})
+
+app.listen(3000, () => {
+  console.log("server listening to requests ...c");
+});
