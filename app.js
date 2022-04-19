628de5aeacb5f3c897f239aa3269edaf65073ed0
Valentin Paul <valentin@vamptech.de>
2022-04-19 17:46:55 +0200

    new html

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express()
 
 app.use(express.static('public'))
 
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + '/views/home.html');
+})
+
 app.get("/about", (req, res, next) => {
   res.sendFile(__dirname + '/views/about.html');
 })
 
 app.get("/gallery", (req, res, next) => {
-  res.sendFile(__dirname + '/views/gallety.html');
+  res.sendFile(__dirname + '/views/gallery.html');
 })
 
 app.listen(3000, () => {
